ef3d1fa53de6812794c81d0d7a7435a847635313
gitapin82y <gitapin82y@gmail.com>
2023-12-06 03:06:47 +0700

    cetak perizinan

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-               
+        Barryvdh\DomPDF\ServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,    
         Yajra\DataTables\DataTablesServiceProvider::class,
 
     ],
@@ -230,6 +231,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class
 
     ],
 
--- resources/views/public/perizinan/form-pengajuan-syarat.blade.php
+++ resources/views/public/perizinan/form-pengajuan-syarat.blade.php
@@ -64,6 +64,7 @@ id="ajukan-perizinan"
 @endsection
 
 @push('extra_script')
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script>
   $(document).ready(function () {
     
