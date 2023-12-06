3bc9dbc8a12d265052af87a72e57452c7ecbc841
gitapin82y <gitapin82y@gmail.com>
2023-12-07 04:37:39 +0700

    ubah main collor

--- resources/views/public/perizinan-pemohon/acc-jadwal.blade.php
+++ resources/views/public/perizinan-pemohon/acc-jadwal.blade.php
@@ -4,7 +4,7 @@
 
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header bg-warning">
+      <div class="modal-header bg-primary">
         <h4 class="modal-title">Penjadwalan Survey</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
@@ -15,8 +15,8 @@
         <h4 class="col-12">Penjadwalan Survey</h4>
 
         <div class="col-12">
-        <button class="btn btn-warning col-12 px-2 mt-4"  id="accJadwalSurvey" type="button">Bersedia</button>
-        <button class="btn btn-light text-warning border border-warning col-12  mt-4 " id="jadwalUlang">Jadwal Ulang</button>
+        <button class="btn btn-primary col-12 px-2 mt-4"  id="accJadwalSurvey" type="button">Bersedia</button>
+        <button class="btn btn-light text-primary border border-primary col-12  mt-4 " id="jadwalUlang">Jadwal Ulang</button>
       </div>
          
         <p class="col-12 mt-4">NB: Anda dapat melakukan penjadwalan ulang survey maksimal 1x</p>
--- resources/views/public/perizinan-pemohon/tolak.blade.php
+++ resources/views/public/perizinan-pemohon/tolak.blade.php
@@ -4,14 +4,14 @@
 
     <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header bg-warning">
+      <div class="modal-header bg-primary">
         <h4 class="modal-title">Berkas Dikembalikan</h4>
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       <div class="modal-body pt-4">
           <h5>Alasan Dikembalikan</h5>
           <p class="alasan_dikembalikan"></p>
-          <a href="#" class="btn py-3 mt-3 btn-warning w-100 text-white kirimUlang" type="button">Kirim Ulang Perizinan</a>
+          <a href="#" class="btn py-3 mt-3 btn-primary w-100 text-white kirimUlang" type="button">Kirim Ulang Perizinan</a>
       </div>
       </div>
 
